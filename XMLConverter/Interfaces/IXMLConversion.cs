0e0b14cb28c79fe35ca287e446e11e2f23a62c38
James Thompson <JT.93@hotmail.com>
2018-12-01 07:23:34 +0000

    First Pass at Documenting everything

--- XMLConverter/Interfaces/IXMLConversion.cs
+++ XMLConverter/Interfaces/IXMLConversion.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace WeMicroIt.Utils.XMLConverter.Interfaces
 {
+    /// <include file='./docs/Interfaces.xml' path='doc/interfaces[@name="IXMLConversion"]/interface/*'/> 
     public interface IXMLConversion
     {
     }
--- XMLConverter/XMLConversion.cs
+++ XMLConverter/XMLConversion.cs
@@ -11,21 +11,109 @@ using WeMicroIt.Utils.XMLConverter.Interfaces;
 
 namespace WeMicroIt.Utils.XMLConverter
 {
+    /// <include file='./docs/Classes.xml' path='doc/classes[@name="XMLConversion"]/class/*'/> 
     public class XMLConversion : IXMLConversion
     {
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="XMLConversion"]/constructor[@name="Default"]/*'/> 
         public XMLConversion()
         {
         }
-        
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="XMLConversion"]/methods[@name="Transform"][@version="Default"]/*'/> 
         public bool Transforms(string sourcePath, string templatePath, string destinationPath)
         {            
             return Transforms(templatePath, sourcePath, destinationPath, new XmlWriterSettings());
         }
 
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="XMLConversion"]/methods[@name="Transform"][@version="Settings"]/*'/> 
         public bool Transforms(string sourcePath, string templatePath, string destinationPath, XmlWriterSettings settings)
         {
             transform(templatePath, sourcePath, destinationPath, settings);
             return true;
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="XMLConversion"]/methods[@name="Split"][@version="Pure"]/*'/> 
+        public List<XElement> PureSplits(string path, string sourcePath)
+        {
+            if (string.IsNullOrEmpty(sourcePath))
+            {
+                throw new FileNotFoundException();
+            }
+            var doc = XDocument.Load(sourcePath);
+            var element = doc.Elements();
+            if (!string.IsNullOrEmpty(path) && !path.Contains('/'))
+            {
+                element = element.Elements(XName.Get(path));
+            }
+            if (path.Contains('/'))
+            {
+                foreach (var item in path.Split('/'))
+                {
+                    element = element.Elements(XName.Get(item));
+                }
+            }
+            var items = element.ToList();
+            if (items.Count < 1)
+            {
+                throw new KeyNotFoundException();
+            }
+            return items;
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="XMLConversion"]/methods[@name="Split"][@version="Group"]/*'/> 
+        public List<XElement> GroupSplits(string path, string sourcePath, string grouping)
+        {
+            if (grouping.Contains('/'))
+            {
+                throw new NotSupportedException();
+            }
+            var items = PureSplits(path, sourcePath);
+            List<XElement> groups = new List<XElement>();
+            List<string> keys = items.Where(x => x.Element(XName.Get("type")).Value.ToLower() == "t").Select(x => x.Element(XName.Get(grouping)).Value.ToString()).Distinct().ToList();
+            foreach (var item in keys)
+            {
+                XElement ele = new XElement(XName.Get("class"));
+                XElement temp = new XElement(XName.Get("name"))
+                {
+                    Value = item
+                };
+                ele.Add(temp);
+                temp = new XElement(XName.Get("attributes"));
+                temp.Add(items.Where(x => x.Element(XName.Get(grouping)).Value.ToLower().StartsWith(item.ToLower())).ToList());
+                ele.Add(temp);
+                groups.Add(ele);
+            }
+            return groups;
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="XMLConversion"]/methods[@name="DeserializeDoc"][@version="General"]/*'/> 
+        public XDocument DeSerializeDocument(string path)
+        {
+            return loadDocument(path);
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="XMLConversion"]/methods[@name="DeserializeObj"][@version="Path"]/*'/> 
+        public T DeserializeObjects<T>(string path)
+        {
+            return DeserializeObjects<T>(loadDocument(path));
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="XMLConversion"]/methods[@name="DeserializeObj"][@version="Document"]/*'/> 
+        public T DeserializeObjects<T>(XDocument doc)
+        {
+            return DeserializeObjects<T>(doc);
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="XMLConversion"]/methods[@name="DeserializeObj"][@version="Object"]/*'/> 
+        public T DeSerializeObjects<T>(XObject content)
+        {
+            return convertToObject<T>(content);
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="XMLConversion"]/methods[@name="SerializeObj"]/*'/> 
+        public string SerializeObjects(object content)
+        {
+            throw new NotImplementedException();
         }
 
         private void transform(string template, string source, string destination)
@@ -74,59 +162,7 @@ namespace WeMicroIt.Utils.XMLConverter
             return xslt;
         }
 
-        public List<XElement> PureSplits(string path, string sourcePath)
-        {
-            if (string.IsNullOrEmpty(sourcePath))
-            {
-                throw new FileNotFoundException();
-            }
-            var doc = XDocument.Load(sourcePath);
-            var element = doc.Elements();
-            if (!string.IsNullOrEmpty(path) && !path.Contains('/'))
-            {
-                element = element.Elements(XName.Get(path));
-            }
-            if (path.Contains('/'))
-            {
-                foreach (var item in path.Split('/'))
-                {
-                    element = element.Elements(XName.Get(item));
-                }
-            }
-            var items = element.ToList();
-            if (items.Count < 1)
-            {
-                throw new KeyNotFoundException();
-            }
-            return items;
-        }
-
-        public List<XElement> GroupSplits(string path, string sourcePath, string grouping)
-        {
-            if (grouping.Contains('/'))
-            {
-                throw new NotSupportedException();
-            }
-            var items = PureSplits(path, sourcePath);
-            List<XElement> groups = new List<XElement>();
-            List<string> keys = items.Where(x => x.Element(XName.Get("type")).Value.ToLower() == "t").Select(x => x.Element(XName.Get(grouping)).Value.ToString()).Distinct().ToList();
-            foreach (var item in keys)
-            {
-                XElement ele = new XElement(XName.Get("class"));
-                XElement temp = new XElement(XName.Get("name"))
-                {
-                    Value = item
-                };
-                ele.Add(temp);
-                temp = new XElement(XName.Get("attributes"));
-                temp.Add(items.Where(x => x.Element(XName.Get(grouping)).Value.ToLower().StartsWith(item.ToLower())).ToList());
-                ele.Add(temp);
-                groups.Add(ele);
-            }
-            return groups;
-        }
-
-        public T DeSerializeObjects<T>(string path)
+        private XDocument loadDocument(string path)
         {
             if (string.IsNullOrEmpty(path))
             {
@@ -137,10 +173,10 @@ namespace WeMicroIt.Utils.XMLConverter
             {
                 throw new InvalidDataException();
             }
-            return DeSerializeObjects<T>(doc.FirstNode);
+            return doc;
         }
 
-        public T DeSerializeObjects<T>(XObject content)
+        private t convertToObject<t>(XObject content)
         {
             if (content == null)
             {
@@ -151,12 +187,7 @@ namespace WeMicroIt.Utils.XMLConverter
             {
                 throw new ArgumentNullException();
             }
-            return JsonConvert.DeserializeObject<T>(json);
-        }
-
-        public string SerializeObjects(object content)
-        {
-            throw new NotImplementedException();
+            return JsonConvert.DeserializeObject<t>(json);
         }
     }
 }
