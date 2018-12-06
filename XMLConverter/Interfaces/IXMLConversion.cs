edcfbff0b7e26b6a37ed3a875e01ff71209f4ccd
James Thompson <thompson.tomo@outlook.com>
2018-12-06 16:46:45 +1100

    Initial Build of Features. (#1)
    Commits
    
    * Add project files.
    
    * Started implementing all features
    
    * Slight tweak to improve logic
    
    * Version bump
    
    * Standardised Frameworks
    
    * Version Bump
    
    * Rename. Got it working when no attributes but have started on a solution using XLST transformation
    
    * refactor
    
    * Additional check
    
    * tidy up and name simplification
    
    * Major refactor to increase re-usability
    
    * Renamed assembly
    
    * First Pass at Documenting everything
    
    * Added Logo

---
+++ XMLConverter/Interfaces/IXMLConversion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeMicroIt.Utils.XMLConverter.Interfaces
+{
+    /// <include file='./docs/Interfaces.xml' path='doc/interfaces[@name="IXMLConversion"]/interface/*'/> 
+    public interface IXMLConversion
+    {
+    }
+}
